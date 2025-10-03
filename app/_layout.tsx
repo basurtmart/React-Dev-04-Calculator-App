2c09e078875f16e143c0f34be2a06fc6312e02e2
mbasurto <mbd.97@hotmail.com>
2025-10-03 00:15:23 -0600

    Fuentes personalizadas y diseño inicial

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,25 @@
-import { View, Text } from 'react-native'
+import { useFonts } from 'expo-font'
+import { View } from 'react-native'
 import React from 'react'
 import { Slot } from 'expo-router'
+import { Colors } from '@/constants/Colors'
+import { StatusBar } from 'expo-status-bar'
 
 const RootLayout = () => {
-  return (
-    <View>
-      <Text>Header</Text>
 
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  })
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <View style={{ backgroundColor: Colors.background, flex: 1 }}>
       <Slot />
 
-      <Text>Footer</Text>
+      <StatusBar style='light' />
     </View>
   )
 }
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,1 +1,10 @@
-export const Colors = {};
+export const Colors = {
+    darkGray: '#2D2D2D',
+    lightGray: '#9B9B9B',
+    orange: '#FF9427',
+    white: '#FFFFFF',
+
+    textPrimary: 'white',
+    textSecondary: '#666666',
+    background: '#000000',
+} as const;
