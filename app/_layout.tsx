023146bb7cdb84be583c08f00a3df72290340655
mbasurto <mbd.97@hotmail.com>
2025-10-03 22:21:32 -0600

    Configuración de Haptics y NavigationBar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Platform, View } from 'react-native'
 
 import { useFonts } from 'expo-font'
 import { Slot } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 
 import { globalStyles } from '@/styles/global-styles'
+
+import * as NavigationBar from 'expo-navigation-bar'
+
+const isAndroid = Platform.OS === 'android'
+if (isAndroid) {
+  NavigationBar.setBackgroundColorAsync('#000000');
+}
 
 const RootLayout = () => {
 
--- components/CalculatorButton.tsx
+++ components/CalculatorButton.tsx
@@ -1,5 +1,6 @@
 import { Colors } from '@/constants/Colors';
 import { globalStyles } from '@/styles/global-styles';
+import * as Haptics from 'expo-haptics';
 import React from 'react';
 import { Pressable, Text } from 'react-native';
 
@@ -11,20 +12,23 @@ interface Props {
     onPress: () => void;
 }
 
-const CalculatorButton = ({ 
-    label, 
-    color = Colors.darkGray, 
-    doubleSize = false, 
-    blackText = false, 
+const CalculatorButton = ({
+    label,
+    color = Colors.darkGray,
+    doubleSize = false,
+    blackText = false,
     onPress,
- }: Props) => {
+}: Props) => {
     return (
         <Pressable style={({ pressed }) => ({
             ...globalStyles.button,
             backgroundColor: color,
             opacity: pressed ? 0.8 : 1,
             width: doubleSize ? 180 : 80,
-        })} onPress={onPress}>
+        })} onPress={() => {
+            Haptics.selectionAsync();
+            onPress();
+        }}>
             <Text style={{
                 ...globalStyles.buttonText,
                 color: blackText ? 'black' : 'white',
