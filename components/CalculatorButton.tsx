e6602f13a77cae13216ca1ccc425f00f92a89e43
mbasurto <mbd.97@hotmail.com>
2025-10-03 21:53:22 -0600

    Bottones de la calculadora

---
+++ components/CalculatorButton.tsx
@@ -0,0 +1,36 @@
+import { Colors } from '@/constants/Colors';
+import { globalStyles } from '@/styles/global-styles';
+import React from 'react';
+import { Pressable, Text } from 'react-native';
+
+interface Props {
+    label: string;
+    color?: string;
+    doubleSize?: boolean;
+    blackText?: boolean;
+    onPress: () => void;
+}
+
+const CalculatorButton = ({ 
+    label, 
+    color = Colors.darkGray, 
+    doubleSize = false, 
+    blackText = false, 
+    onPress,
+ }: Props) => {
+    return (
+        <Pressable style={({ pressed }) => ({
+            ...globalStyles.button,
+            backgroundColor: color,
+            opacity: pressed ? 0.8 : 1,
+            width: doubleSize ? 180 : 80,
+        })} onPress={onPress}>
+            <Text style={{
+                ...globalStyles.buttonText,
+                color: blackText ? 'black' : 'white',
+            }}>{label}</Text>
+        </Pressable>
+    )
+}
+
+export default CalculatorButton
